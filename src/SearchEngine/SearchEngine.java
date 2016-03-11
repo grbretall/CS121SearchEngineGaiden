a9811a1bf8e594ea47fc822ec6599071bb34cf77
gbretall <grbretall@gmail.com>
2016-03-11 14:19:43 -0800

    Moved frequency stuff over to utilities

--- src/SearchEngine/SearchEngine.java
+++ src/SearchEngine/SearchEngine.java
@@ -1,10 +1,14 @@
 package SearchEngine;
 
+import Utilities.FrequencyFloatMap;
+
 public class SearchEngine 
 {
+	FrequencyFloatMap searchResults;
+	
 	
 	public SearchEngine()
 	{
-		
+		searchResults = new FrequencyFloatMap();
 	}
 }
---
+++ src/Utilities/FrequencyFloatMap.java
@@ -0,0 +1,69 @@
+package Utilities;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FrequencyFloatMap 
+{
+
+	private HashMap<String, Double> wordFrequencyFloatMap;
+	private HashSet<String> excludedWords = new HashSet<String>();
+	
+	public FrequencyFloatMap()
+	{
+		wordFrequencyFloatMap  = new HashMap<String, Double>();
+		excludedWords = new HashSet<String>();
+	}
+	
+	public void addToMap(String token)
+	{
+		if(excludedWords.contains(token) || token.length() < 2)
+		{
+			return;
+		}
+		else if(wordFrequencyFloatMap.containsKey(token))
+		{
+			wordFrequencyFloatMap.put(token, wordFrequencyFloatMap.get(token)+1);
+		}
+		else
+		{
+			wordFrequencyFloatMap.put(token, 1.0);
+		}
+	}
+	
+	public void addToMap(String token, double value){
+		//System.out.println("token:"+ token+ " value:"+ value);
+		wordFrequencyFloatMap.put(token, value);
+	}
+	
+	public HashMap<String, Double> getMap()
+	{
+		return wordFrequencyFloatMap;
+	}
+	
+	public double getFrequency(String key)
+	{
+		return wordFrequencyFloatMap.get(key);
+	}
+	
+	public void setExcludedWords(HashSet<String> toSet){
+		excludedWords = toSet;
+	}
+	
+	public void clear()
+	{
+		wordFrequencyFloatMap.clear();
+	}
+	
+	public ArrayList<FrequencyFloat> sort()
+	{
+		ArrayList<FrequencyFloat> returnList = new ArrayList<FrequencyFloat>();
+		//Comparator new FrequencyComparator();
+		wordFrequencyFloatMap.forEach((key, value) -> 
+				returnList.add(new FrequencyFloat(key, value)));
+		Collections.sort(returnList, new FrequencyFloatComparator());
+		return returnList;
+	}
+}
